275a8218fc9657a5cf9da6c3717f3a280ac26e1c
ya4615 <ya4615@naver.com>
2018-08-10 02:59:07 +0900

    data loader (TODO:  make sure it's running)

--- spatial_dataloader.py
+++ spatial_dataloader.py
@@ -1,6 +1,7 @@
 import random
 import os
 import cv2
+import re
 
 
 class spatial_dataset(Dataset):
@@ -25,14 +26,15 @@ class spatial_dataset(Dataset):
             nb_frame = self.values[cur_key][0]
             self.clips_idx = random.randint(1, int(nb_frame))
         elif self.mode == 'val':
-            nb_frame = self.values[cur_key][0]
             self.video = cur_key.split('/')[0]
+            self.clips_idx = int(cur_key.split('-')[1])
         else:
             raise ValueError('There are only train and val mode')
 
         label = self.values[cur_key][1]
         data_root = os.path.join(self.root_dir, self.keys[idx])
         cur_data_list = os.listdir(data_root)
+
         data_path = os.path.join(data_root, cur_data_list[self.clips_idx-1])
         data = cv2.imread(data_path)
 
@@ -50,41 +52,37 @@ class Spatial_DataLoader():
 
         self.BATCH_SIZE = BATCH_SIZE
         self.num_workers = num_workers
-        self.frame_count = {}
+        # self.frame_count = {}
         self.in_channel = in_channel
         self.data_path = path
         self.text_path = txt_path
         self.split_num = split_num
         # split the training and testing videos
-        splitter = UCF101_splitter(path=ucf_list, split=ucf_split)
-        self.train_video, self.test_video = splitter.split_video()
+        self.train_video, self.test_video = self.load_train_test_list()
 
     @staticmethod
     def read_text_file(file_path):
+        tmp = {}
         f = open(file_path, 'r')
         for line in f.readlines():
-            split_line = line.split
+            line = line.replace('\n', '')
+            split_line = line.split(" ")
+            tmp[split_line[0]] = [int(split_line[1]), int(split_line[2])]  # split[0] is video name and split[1] and [2] are frame num and class label
+
+        return tmp
 
     def load_train_test_list(self):
-        test_file_path = os.path.join(self.text_path, "test_split%d.txt" % self.split_num)
         train_file_path = os.path.join(self.text_path, "train_split%d.txt" % self.split_num)
+        test_file_path = os.path.join(self.text_path, "test_split%d.txt" % self.split_num)
 
-    def load_frame_count(self):
-        # print '==> Loading frame number of each video'
-        with open('dic/frame_count.pickle', 'rb') as file:
-            dic_frame = pickle.load(file)
-        file.close()
+        train_video = self.read_text_file(train_file_path)
+        test_video = self.read_text_file(test_file_path)
 
-        for line in dic_frame:
-            videoname = line.split('_', 1)[1].split('.', 1)[0]
-            n, g = videoname.split('_', 1)
-            if n == 'HandStandPushups':
-                videoname = 'HandstandPushups_' + g
-            self.frame_count[videoname] = dic_frame[line]
+        return train_video, test_video
 
     def run(self):
-        self.load_frame_count()
-        self.get_training_dic()
+        # self.load_frame_count()
+        # self.get_training_dic()
         self.val_sample19()
         train_loader = self.train()
         val_loader = self.val()
@@ -93,32 +91,23 @@ class Spatial_DataLoader():
 
     def val_sample19(self):
         self.dic_test_idx = {}
-        # print len(self.test_video)
         for video in self.test_video:
-            n, g = video.split('_', 1)
-
-            sampling_interval = int((self.frame_count[video] - 10 + 1) / 19)
+            sampling_interval = int((self.test_video[video][0] - 10 + 1) / 19)
             for index in range(19):
                 clip_idx = index * sampling_interval
                 key = video + '-' + str(clip_idx + 1)
                 self.dic_test_idx[key] = self.test_video[video]
 
-    def get_training_dic(self):
-        self.dic_video_train = {}
-        for video in self.train_video:
-            nb_clips = self.frame_count[video] - 10 + 1
-            key = video + '-' + str(nb_clips)
-            self.dic_video_train[key] = self.train_video[video]
-
     def train(self):
-        training_set = motion_dataset(dic=self.dic_video_train, in_channel=self.in_channel, root_dir=self.data_path,
+        training_set = motion_dataset(dic=self.train_video, in_channel=self.in_channel, root_dir=self.data_path,
                                       mode='train',
                                       transform=transforms.Compose([
-                                          transforms.Scale([224, 224]),
+                                          transforms.RandomCrop(224),
+                                          transforms.RandomHorizontalFlip(),
                                           transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                       ]))
-        print
-        '==> Training data :', len(training_set), ' videos', training_set[1][0].size()
+        print('==> Training data :', len(training_set), ' videos', training_set[1][0].size())
 
         train_loader = DataLoader(
             dataset=training_set,
@@ -136,9 +125,9 @@ class Spatial_DataLoader():
                                         transform=transforms.Compose([
                                             transforms.Scale([224, 224]),
                                             transforms.ToTensor(),
+                                            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                         ]))
-        print
-        '==> Validation data :', len(validation_set), ' frames', validation_set[1][1].size()
+        print('==> Validation data :', len(validation_set), ' frames', validation_set[1][1].size())
         # print validation_set[1]
 
         val_loader = DataLoader(
--- temporal_dataloader.py
+++ temporal_dataloader.py
@@ -1,5 +1,7 @@
 import random
-
+import os
+import cv2
+import re
 
 class motion_dataset(Dataset):
     def __init__(self, dic, in_channel, root_dir, mode, transform=None):
@@ -13,31 +15,32 @@ class motion_dataset(Dataset):
         self.img_rows = 224
         self.img_cols = 224
 
-    def stackopf(self):
-        name = 'v_' + self.video
-        u = self.root_dir + 'u/' + name
-        v = self.root_dir + 'v/' + name
+    def stackopf(self, keys):
+        video_path = os.path.join(self.root_dir, keys)
+        # name = 'v_' + self.video
+        # u = self.root_dir + 'u/' + name
+        # v = self.root_dir + 'v/' + name
 
         flow = torch.FloatTensor(2 * self.in_channel, self.img_rows, self.img_cols)
         i = int(self.clips_idx)
 
         for j in range(self.in_channel):
             idx = i + j
-            idx = str(idx)
-            frame_idx = 'frame' + idx.zfill(6)
-            h_image = u + '/' + frame_idx + '.jpg'
-            v_image = v + '/' + frame_idx + '.jpg'
+            frame_idx = "%05d.jpg" % idx
+            # frame_idx = 'frame' + idx.zfill(6)
+            x_image = 'flow_x_' + frame_idx
+            y_image = 'flow_y_' + frame_idx
 
-            imgH = (Image.open(h_image))
-            imgV = (Image.open(v_image))
+            imgX = (Image.open(x_image))
+            imgY = (Image.open(y_image))
 
-            H = self.transform(imgH)
-            V = self.transform(imgV)
+            X = self.transform(imgX)
+            Y = self.transform(imgY)
 
-            flow[2 * (j - 1), :, :] = H
-            flow[2 * (j - 1) + 1, :, :] = V
-            imgH.close()
-            imgV.close()
+            flow[2 * (j - 1), :, :] = X
+            flow[2 * (j - 1) + 1, :, :] = Y
+            imgX.close()
+            imgY.close()
         return flow
 
     def __len__(self):
@@ -45,18 +48,19 @@ class motion_dataset(Dataset):
 
     def __getitem__(self, idx):
         # print ('mode:',self.mode,'calling Dataset:__getitem__ @ idx=%d'%idx)
-
+        cur_key = self.keys[idx]
         if self.mode == 'train':
-            self.video, nb_clips = self.keys[idx].split('-')
-            self.clips_idx = random.randint(1, int(nb_clips))
+            nb_frame = self.values[cur_key][0]
+            self.clips_idx = random.randint(1, int(nb_frame))
+            self.video = cur_key.split('/')[0]
         elif self.mode == 'val':
-            self.video, self.clips_idx = self.keys[idx].split('-')
+            self.video = cur_key.split('/')[0]
+            self.clips_idx = int(cur_key.split('-')[1])
         else:
             raise ValueError('There are only train and val mode')
 
-        label = self.values[idx]
-        label = int(label) - 1
-        data = self.stackopf()
+        label = self.values[cur_key][1]
+        data = self.stackopf(cur_key)
 
         if self.mode == 'train':
             sample = (data, label)
@@ -68,33 +72,41 @@ class motion_dataset(Dataset):
 
 
 class Motion_DataLoader():
-    def __init__(self, BATCH_SIZE, num_workers, in_channel, path, ucf_list, ucf_split):
+    def __init__(self, BATCH_SIZE, num_workers, in_channel, path, txt_path, split_num):
 
         self.BATCH_SIZE = BATCH_SIZE
         self.num_workers = num_workers
         self.frame_count = {}
         self.in_channel = in_channel
         self.data_path = path
+        self.text_path = txt_path
+        self.split_num = split_num
         # split the training and testing videos
-        splitter = UCF101_splitter(path=ucf_list, split=ucf_split)
-        self.train_video, self.test_video = splitter.split_video()
+        self.train_video, self.test_video = self.load_train_test_list()
 
-    def load_frame_count(self):
-        # print '==> Loading frame number of each video'
-        with open('dic/frame_count.pickle', 'rb') as file:
-            dic_frame = pickle.load(file)
-        file.close()
+    @staticmethod
+    def read_text_file(file_path):
+        tmp = {}
+        f = open(file_path, 'r')
+        for line in f.readlines():
+            line = line.replace('\n', '')
+            split_line = line.split(" ")
+            tmp[split_line[0]] = [int(split_line[1]), int(split_line[2])]  # split[0] is video name and split[1] and [2] are frame num and class label
 
-        for line in dic_frame:
-            videoname = line.split('_', 1)[1].split('.', 1)[0]
-            n, g = videoname.split('_', 1)
-            if n == 'HandStandPushups':
-                videoname = 'HandstandPushups_' + g
-            self.frame_count[videoname] = dic_frame[line]
+        return tmp
+
+    def load_train_test_list(self):
+        train_file_path = os.path.join(self.text_path, "train_split%d.txt" % self.split_num)
+        test_file_path = os.path.join(self.text_path, "test_split%d.txt" % self.split_num)
+
+        train_video = self.read_text_file(train_file_path)
+        test_video = self.read_text_file(test_file_path)
+
+        return train_video, test_video
 
     def run(self):
-        self.load_frame_count()
-        self.get_training_dic()
+        # self.load_frame_count()
+        # self.get_training_dic()
         self.val_sample19()
         train_loader = self.train()
         val_loader = self.val()
@@ -103,32 +115,23 @@ class Motion_DataLoader():
 
     def val_sample19(self):
         self.dic_test_idx = {}
-        # print len(self.test_video)
         for video in self.test_video:
             n, g = video.split('_', 1)
 
-            sampling_interval = int((self.frame_count[video] - 10 + 1) / 19)
+            sampling_interval = int((self.test_video[video][0] - 10 + 1) / 19)
             for index in range(19):
                 clip_idx = index * sampling_interval
                 key = video + '-' + str(clip_idx + 1)
                 self.dic_test_idx[key] = self.test_video[video]
 
-    def get_training_dic(self):
-        self.dic_video_train = {}
-        for video in self.train_video:
-            nb_clips = self.frame_count[video] - 10 + 1
-            key = video + '-' + str(nb_clips)
-            self.dic_video_train[key] = self.train_video[video]
-
     def train(self):
-        training_set = motion_dataset(dic=self.dic_video_train, in_channel=self.in_channel, root_dir=self.data_path,
+        training_set = motion_dataset(dic=self.train_video, in_channel=self.in_channel, root_dir=self.data_path,
                                       mode='train',
                                       transform=transforms.Compose([
                                           transforms.Scale([224, 224]),
                                           transforms.ToTensor(),
                                       ]))
-        print
-        '==> Training data :', len(training_set), ' videos', training_set[1][0].size()
+        print('==> Training data :', len(training_set), ' videos', training_set[1][0].size())
 
         train_loader = DataLoader(
             dataset=training_set,
@@ -147,8 +150,7 @@ class Motion_DataLoader():
                                             transforms.Scale([224, 224]),
                                             transforms.ToTensor(),
                                         ]))
-        print
-        '==> Validation data :', len(validation_set), ' frames', validation_set[1][1].size()
+        print('==> Validation data :', len(validation_set), ' frames', validation_set[1][1].size())
         # print validation_set[1]
 
         val_loader = DataLoader(
