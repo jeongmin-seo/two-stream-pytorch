2a0a1545714472e5988d9b644570dea779607a0e
ya4615 <ya4615@naver.com>
2018-09-18 15:35:39 +0900

    modify generator

--- cube_gan.py
+++ cube_gan.py
@@ -3,13 +3,15 @@ import torch.optim as optim
 import torch.nn as nn
 import torch
 import spatial_cube_dataloader as data_loader
+from util import save_best_model
+import numpy as np
 
 cuda = True if torch.cuda.is_available() else False
 
 # experimental parameters
 data_root = "/home/jm/Two-stream_data/HMDB51/original/frames"
 txt_root = "/home/jm/Two-stream_data/HMDB51"
-save_path = "/home/jm/workspace/two-stream-pytorch/spatial_cube_model"
+save_path = "/home/jm/workspace/two-stream-pytorch/cube_gan_model"
 batch_size = 1
 nb_epoch = 10000
 L = 16
@@ -20,38 +22,30 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         # self.init_size = opt.img_size // 4
-        self.init_size = 3
-        # self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
-        self.l1 = nn.Sequential(nn.Linear(5000, 128 * self.init_size * self.init_size * self.init_size))
+        self.init_size = 6
+        self.init_channel = 4
+        self.l1 = nn.Sequential(nn.Linear(5000, 512 * self.init_channel * self.init_size * self.init_size))
 
         self.conv_blocks = nn.Sequential(
-            nn.ConvTranspose3d(128, 512, kernel_size=3),
-            nn.ReLU(),
-
             nn.ConvTranspose3d(512, 256, kernel_size=3),
-            nn.ReLU(),
-            # nn.MaxUnpool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.ReLU(True),
 
             nn.ConvTranspose3d(256, 256, kernel_size=3),
-            nn.ReLU(),
+            nn.ReLU(True),
 
+            nn.ConvTranspose3d(256, 128, kernel_size=(3, 5, 5), stride=(1,2,2)),
+            nn.ReLU(True),
 
-            nn.ConvTranspose3d(256, 128, kernel_size=3),
-            nn.ReLU(),
-            # nn.MaxUnpool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.ConvTranspose3d(128, 64, kernel_size=(3, 9, 9), stride=(1,2,2)),
+            nn.ReLU(True),
 
-            nn.ConvTranspose3d(128, 64, kernel_size=3),
-            nn.ReLU(),
-            # nn.MaxUnpool3d(kernel_size=(1, 2, 2), stride=(2, 2, 2)),
-
-            nn.ConvTranspose3d(64, 3, kernel_size=5),
-
+            nn.ConvTranspose3d(64, 3, kernel_size=(5, 8, 8), stride=(1,2,2)),
             nn.Tanh()
         )
 
     def forward(self, z):
         out = self.l1(z)
-        out = out.view(out.shape[0], 128, self.init_size, self.init_size, self.init_size)
+        out = out.view(out.shape[0], 512, self.init_channel, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
 
@@ -63,17 +57,14 @@ class Discriminator(nn.Module):
         self.model = nn.Sequential(
             nn.Conv3d(3, 64, kernel_size=(5,7,7), stride=(1,2,2)),
             nn.ReLU(),
-            # nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),
 
             nn.Conv3d(64, 128, kernel_size=(3,7,7), stride=(1,2,2)),
             nn.ReLU(),
-            # nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
 
             nn.Conv3d(128, 256, kernel_size=(3,5,5), stride=(1,2,2)),
             nn.ReLU(),
             nn.Conv3d(256, 256, kernel_size=3),
             nn.ReLU(),
-            # nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
 
             nn.Conv3d(256, 512, kernel_size=3),
             nn.ReLU()
@@ -85,9 +76,9 @@ class Discriminator(nn.Module):
 
     def forward(self, img_cube):
         out = self.model(img_cube)
-        print(out.size())
+        # print(out.size())
         out = out.view(out.size(0), -1)
-        print(out.size())
+        # print(out.size())
         validity = self.adv_layer(out)
 
         return validity
@@ -113,33 +104,43 @@ if __name__ == "__main__":
     optimizerD = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
     optimizerG = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
-    for i, (data, label) in enumerate(train_loader):
+    prev_err = None
+    for epoch in range(nb_epoch):
+        generator_error = []
+        for i, (data, label) in enumerate(train_loader):
 
-        # discriminator train
-        discriminator.zero_grad()
+            # discriminator train
+            discriminator.zero_grad()
 
-        input_var = Variable(data).cuda()
-        target = Variable(torch.ones(input_var.size()[0])).cuda()
-        output = discriminator(input_var)
-        err_real = criterion(output, target)
+            input_var = Variable(data).cuda()
+            target = Variable(torch.ones(input_var.size()[0])).cuda()
+            output = discriminator(input_var)
+            err_real = criterion(output, target)
 
-        # noise = Variable(torch.randn(input_var.size()[0], latent_Vector, 1, 1)).cuda()
-        noise = Variable(torch.randn(input_var.size()[0], 5000)).cuda()
-        fake = generator(noise)
-        target = Variable(torch.zeros(input_var.size()[0]))
-        output = discriminator(fake.detach())
-        err_fake = criterion(output, target)
+            # noise = Variable(torch.randn(input_var.size()[0], latent_Vector, 1, 1)).cuda()
+            noise = Variable(torch.randn(input_var.size()[0], 5000)).cuda()
+            fake = generator(noise)
+            target = Variable(torch.zeros(input_var.size()[0])).cuda()
+            output = discriminator(fake.detach())
+            err_fake = criterion(output, target)
 
-        errD = err_real + err_fake
-        errD.backward()
-        optimizerD.step()
+            errD = err_real + err_fake
+            errD.backward()
+            optimizerD.step()
 
-        # generator train
-        generator.zero_grad()
-        target = Variable(torch.ones(input_var.size()[0]))
-        output = discriminator(fake)
-        errG = criterion(output, target)
-        errG.backward()
-        optimizerG.step()
+            # generator train
+            generator.zero_grad()
+            target = Variable(torch.ones(input_var.size()[0])).cuda()
+            output = discriminator(fake)
+            errG = criterion(output, target)
+            errG.backward()
+            optimizerG.step()
 
-        # print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, 25, i, len(train_loader), errD.data[0], errG.data[0]))
+            print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, nb_epoch, i, len(train_loader), errD.data[0], errG.data[0]))
+            generator_error.append(errG.data[0])
+
+        model_err = np.mean(np.asarray(generator_error))
+        if not prev_err or prev_err > model_err:
+            save_best_model(True, generator, save_path, epoch)
+            prev_err = model_err
+
