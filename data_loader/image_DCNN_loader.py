5f403aba32bd90849eff8dcdb9d95f6144f2da3e
jeongmin <ya4615@naver.com>
2018-10-29 02:00:18 +0900

    add image dcnn model(still editing)

---
+++ data_loader/image_DCNN_loader.py
@@ -0,0 +1,118 @@
+import os
+import re
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+
+
+class DCNNDataset(Dataset):
+
+    def __init__(self, dic, root_dir, transform=None):
+        self.keys = list(dic.keys())
+        self.dic = dic
+        self.root_dir = root_dir
+        self.transform = transform
+        self.max_frame_num = 1081 # 수정하기
+        self.img_size = 224
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        cur_video_key = self.keys[idx]
+        cur_video_path = os.path.join(self.root_dir, cur_video_key)
+        nb_frame = self.dic[cur_video_key][0]
+        label = int(self.dic[cur_video_key][1])
+
+        cube = torch.FloatTensor(3, self.max_frame_num, self.img_size, self.img_size)
+
+        j = 0
+        for file_name in sorted(os.listdir(cur_video_path)):
+            if not re.split('[.]+', file_name)[-1] == 'jpg':
+                continue
+
+            img = (Image.open(os.path.join(cur_video_path, file_name)))
+            X = self.transform(img)
+            cube[:, j, :, :] = X
+            j = j + 1
+            img.close()
+
+        return X, label, cur_video_key
+
+
+class DCNNLoader:
+
+    def __init__(self, batch_size, num_workers, path, txt_path, split_num):
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.data_path = path
+        self.text_path = txt_path
+        self.split_num = split_num
+
+        # split the training and testing videos
+        self.train_video, self.test_video = self.load_train_test_list()
+
+    @staticmethod
+    def read_text_file(file_path):
+        tmp = {}
+        f = open(file_path, 'r')
+        for line in f.readlines():
+            line = line.replace('\n', '')
+            split_line = line.split(" ")
+
+            # split[0] is video name and split[1] and [2] are frame num and class label
+            tmp[split_line[0]] = [int(split_line[1]), int(split_line[2])]
+
+        return tmp
+
+    def load_train_test_list(self):
+        train_file_path = os.path.join(self.text_path, "train_split%d.txt" % self.split_num)
+        test_file_path = os.path.join(self.text_path, "test_split%d.txt" % self.split_num)
+
+        train_video = self.read_text_file(train_file_path)
+        test_video = self.read_text_file(test_file_path)
+
+        return train_video, test_video
+
+    def run(self):
+        training_set = DCNNDataset(self.train_video,
+                                   self.data_path,
+                                   transform=transforms.Compose([
+                                       transforms.Scale([224, 224]),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                            std=[0.229, 0.224, 0.225])
+                                   ]))
+        train_loader = DataLoader(training_set, batch_size=self.batch_size, shuffle=False)
+
+        validation_set = DCNNDataset(self.test_video,
+                                     self.data_path,
+                                     transform=transforms.Compose([
+                                         transforms.Scale([224,224]),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                              std=[0.229, 0.224, 0.225])
+                                     ]))
+        val_loader = DataLoader(validation_set, batch_size=self.batch_size, shuffle=False)
+        return train_loader, val_loader
+
+
+if __name__=="__main__":
+
+    """
+    root = "/home/jm/hdd/representation/split1"
+
+    for action_list in os.listdir(root):
+        action_root = os.path.join(root, action_list)
+        for v_list in os.listdir(action_list):
+            video_root = os.path.join(action_root, v_list)
+            for img_list in os.listdir(video_root):
+                dat_path = os.path.join(video_root, img_list)
+
+                np.load(dat_path)
+    """
+
+#  A  A
+# (‘ㅅ‘=)
+# J.M.Seo
---
+++ train/image_DCNN_train.py
@@ -0,0 +1,179 @@
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import data_loader.image_DCNN_loader as data_loader
+import torchvision.models as models
+from network.dynamic_k_max import DynamicKMaxPooling
+import numpy as np
+import visdom
+import pickle
+from util.util import accuracy, frame2_video_level_accuracy, save_best_model
+
+batch_size = 32
+nb_epoch = 10000
+data_root = "/home/jm/hdd/representation/split1"
+text_root = "/home/jm/Two-stream_data/HMDB51"
+save_path = "/home/jm/hdd/dynamic_k_max_model"
+
+
+class ConvTemporal(nn.Module):
+    def __init__(self, batch_size):
+        super(ConvTemporal, self).__init__()
+        self.batch_size = batch_size
+        self.dynamic_k_maxpool1 = DynamicKMaxPooling(16, 5)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(2000, 1700, 3),
+            nn.ReLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(1700, 1400, 3),
+            nn.ReLU()
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(1400, 1000, 3),
+            nn.ReLU()
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(1000, 700, 3),
+            nn.ReLU()
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv1d(700, 400, 3),
+            nn.ReLU()
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(6400, 4000),
+            nn.Linear(4000, 2000),
+            nn.Linear(2000, 51)
+        )
+
+
+    def forward(self, _input):
+        out = self.conv1(_input)
+        out = self.dynamic_k_maxpool1(out, 1)
+        out = self.conv2(out)
+        out = self.dynamic_k_maxpool1(out, 2)
+        out = self.conv3(out)
+        out = self.dynamic_k_maxpool1(out, 3)
+        out = self.conv4(out)
+        out = self.dynamic_k_maxpool1(out, 4)
+        out = self.conv5(out)
+        out = self.dynamic_k_maxpool1(out, 5)
+        out = out.view(out.size()[0], -1)
+
+        out = self.fc(out)
+
+        return out
+
+
+def train_1epoch(_img_feature_extract_model, _dcnn_model, _train_loader, _optimizer, _loss_func, _epoch, _nb_epochs):
+    print('==> Epoch:[{0}/{1}][training stage]'.format(_epoch, _nb_epochs))
+
+    accuracy_list = []
+    loss_list = []
+    _img_feature_extract_model.train()
+    _dcnn_model.train()
+    for i, (dat, label) in enumerate(_train_loader):
+        label = label.cuda()
+        input_var = Variable(dat).cuda()
+        target_var = Variable(label).cuda().long()
+
+        output = _model(input_var)
+
+        loss = _loss_func(output, target_var)
+        loss_list.append(loss.data)
+        accuracy_list.append(accuracy(output.data, label))
+
+        # compute gradient and do SGD step
+        _optimizer.zero_grad()
+        loss.backward()
+        _optimizer.step()
+
+    return float(sum(accuracy_list) / len(accuracy_list)), float(sum(loss_list) / len(loss_list)), _img_feature_extract_model, _dcnn_model
+
+
+def validation_1epoch(_img_feature_extract_model, _dcnn_model, _train_loader, _optimizer, _loss_func, _epoch, _nb_epochs):
+    print('==> Epoch:[{0}/{1}][validation stage]'.format(_epoch, _nb_epochs))
+
+    dic_video_level_preds = {}
+    dic_video_level_targets = {}
+    _model.eval()
+    for i, (video, dat, label) in enumerate(_val_loader):
+        label = label.cuda(async=True)
+        with torch.no_grad():
+            input_var = Variable(dat).cuda(async=True)
+            target_var = Variable(label).cuda(async=True)
+
+        # compute output
+        output = _model(input_var)
+
+        # Calculate video level prediction
+        preds = output.data.cpu().numpy()
+        nb_data = preds.shape[0]
+        for j in range(nb_data):
+            videoName = video[j]
+            if videoName not in dic_video_level_preds.keys():
+                dic_video_level_preds[videoName] = preds[j, :]
+                dic_video_level_targets[videoName] = target_var[j]
+            else:
+                dic_video_level_preds[videoName] += preds[j, :]
+
+    video_acc, video_loss = frame2_video_level_accuracy(dic_video_level_preds, dic_video_level_targets, _criterion)
+
+    return video_acc, video_loss, dic_video_level_preds
+
+
+if __name__=="__main__":
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    vis = visdom.Visdom()
+    train_acc_plot = vis.line(X=np.asarray([0]), Y=np.asarray([0]))
+    train_loss_plot = vis.line(X=np.asarray([0]), Y=np.asarray([0]))
+    val_loss_plot = vis.line(X=np.asarray([0]), Y=np.asarray([0]))
+    val_acc_plot = vis.line(X=np.asarray([0]), Y=np.asarray([0]))
+
+    loader = data_loader.DCNNLoader(batch_size=batch_size, num_workers=4,
+                                    path=data_root, txt_path=text_root, split_num=1)
+    train_loader, val_loader = loader.run()
+
+    feature_extract_model = models.resnet34(pretrained=True)
+    action_decision_model = ConvTemporal(batch_size)
+
+    feature_extract_model.to(device)
+    action_decision_model.to(device)
+    parameters = list(feature_extract_model.parameters()) + list(action_decision_model.parameters())
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.Adam(parameters, betas=(0.5, 0.999), lr=2e-4)
+
+    cur_best_acc = 0
+    for epoch in range(1, nb_epoch +1):
+        train_acc, train_loss, model = train_1epoch(feature_extract_model, action_decision_model,
+                                                    train_loader, optimizer, criterion, epoch, nb_epoch)
+        print("Train Accuacy:", train_acc, "Train Loss:", train_loss)
+        val_acc, val_loss, video_level_pred = validation_1epoch(feature_extract_model, action_decision_model,
+                                                                val_loader, optimizer, criterion, epoch, nb_epoch)
+        print("Validation Accuracy:", val_acc, "Validation Loss:", val_loss)
+
+        is_best = val_acc > cur_best_acc
+        if is_best:
+            cur_best_acc = val_acc
+            with open('../spatial_cube_model/spatial_cube_preds.pickle', 'wb') as f:
+                pickle.dump(video_level_pred, f)
+            f.close()
+
+        vis.line(X=np.asarray([epoch]), Y=np.asarray([train_loss]),
+                 win=train_loss_plot, update="append", name='Train Loss')
+        vis.line(X=np.asarray([epoch]), Y=np.asarray([train_acc]),
+                 win=train_acc_plot, update="append", name="Train Accuracy")
+        vis.line(X=np.asarray([epoch]), Y=np.asarray([val_loss]),
+                 win=val_loss_plot, update="append", name='Validation Loss')
+        vis.line(X=np.asarray([epoch]), Y=np.asarray([val_acc]),
+                 win=val_acc_plot, update="append", name="Validation Accuracy")
+        save_best_model(is_best, model, save_path, epoch)
+
+#  A  A
+# (‘ㅅ‘=)
+# J.M.Seo
